8a909a07b1f14d643f9b0623e16eba02e431bdd2
thomdela2 <thomdela2@student.arteveldehs.be>
2022-06-29 12:26:29 +0200

    ✨ Add first queu

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Jobs\ProcessPodcast;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    // Delay Queu
+    // ProcessPodcast::dispatch()->delay(3);
+
+    //
+    ProcessPodcast::dispatch();
+
     return view('welcome');
 });
